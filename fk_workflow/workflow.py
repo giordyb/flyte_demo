7634903ee7e6e83bc32302c97c89f6a91185c2a3
biagio <giordano.bianchi@zucchetti.it>
2020-01-23 20:17:25 +0100

    fixes

--- fk_workflow/workflow.py
+++ fk_workflow/workflow.py
@@ -6,20 +6,15 @@ from flytekit.sdk.tasks import python_task, outputs, inputs
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Output, Input
 from flytekit.common.tasks.task import SdkTask
-
-
+read_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset","1e0d95cc82b85cdd96feab3c6f9b6e2f7baba216")
+download_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset","1e0d95cc82b85cdd96feab3c6f9b6e2f7baba216")
 @workflow_class
 class myworkflow(object):
     string_in = Input(Types.String, required=True, help="input string")
-    csv_url = "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2018-financial-year-provisional/Download-data/annual-enterprise-survey-2018-financial-year-provisional-csv.csv"
     dataset = Input(Types.CSV, default=Types.CSV.create_at_known_location(
-        csv_url), help="A CSV File")
-
-    download_dataset = SdkTask.fetch("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset","f7fdff3213e101fea7feeb03f5bf88d3f0b0814c")
-    return_dataset = download_dataset(dataset=dataset)
-    read_pickle = SdkTask.fetch("flytedemo","development","fk_tasks.tasks_and_workflow.read_pickle","f7fdff3213e101fea7feeb03f5bf88d3f0b0814c")
-    return_pickle = read_pickle(dataset=return_dataset.outputs.out)
-    
+        "http://172.16.140.171:8000/label_summary.csv"),
+                    help="A CSV File")
+    return_dataset = download_task(dataset=dataset)
+    return_pickle = read_task(dataset=return_dataset.outputs.out)
     myoutput = Output(return_pickle.outputs.csv_head, sdk_type=Types.String)
-
 # %%
