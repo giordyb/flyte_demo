2da691b4e01f124cc43b2de578af14840a3993bc
biagio <giordano.bianchi@zucchetti.it>
2020-01-23 20:26:45 +0100

    fix fetch

--- fk_workflow/workflow.py
+++ fk_workflow/workflow.py
@@ -6,15 +6,22 @@ from flytekit.sdk.tasks import python_task, outputs, inputs
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Output, Input
 from flytekit.common.tasks.task import SdkTask
-read_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset","1e0d95cc82b85cdd96feab3c6f9b6e2f7baba216")
-download_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset","1e0d95cc82b85cdd96feab3c6f9b6e2f7baba216")
+uppercase_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.uppercase_task")
+reverse_task = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.reverse_task")
+read_pickle = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.read_pickle")
+download_dataset = SdkTask.fetch_latest("flytedemo","development","fk_tasks.tasks_and_workflow.download_dataset")
+
+
 @workflow_class
 class myworkflow(object):
     string_in = Input(Types.String, required=True, help="input string")
     dataset = Input(Types.CSV, default=Types.CSV.create_at_known_location(
         "http://172.16.140.171:8000/label_summary.csv"),
                     help="A CSV File")
-    return_dataset = download_task(dataset=dataset)
-    return_pickle = read_task(dataset=return_dataset.outputs.out)
+    return_dataset = download_dataset(dataset=dataset)
+    return_pickle = read_pickle(dataset=return_dataset.outputs.out)
+    upper_out = uppercase_task(input_string=string_in)
+    reversed_out = reverse_task(input_string=upper_out.outputs.output_string)
     myoutput = Output(return_pickle.outputs.csv_head, sdk_type=Types.String)
-# %%
+
+    mystringoutput = Output(reversed_out.outputs.rev_output_string, sdk_type=Types.String)
